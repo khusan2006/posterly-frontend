d558a81bb3e65a83dfedf512f0c0f86850ebd901
Khusan <bahodirovkhusan@gmail.com>
2024-03-16 12:59:48 +0500

    updated build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  build: {
+    minify: true,
+    sourcemap: false,
+    outDir: "dist",
+    assetsDir: ".",
+    cssCodeSplit: true,
+    rollupOptions: {
+      external: ["react", "react-dom"],
     },
   },
 });
